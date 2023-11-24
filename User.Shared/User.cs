101f5ea7b155df5fd527dd77efb48610ed7c4f4c
Pedro Armando Navarrete Deodanes <202102186@ujmd.edu.sv>
2023-11-24 17:36:31 -0600

    Agregar archivos de proyecto.

---
+++ User.Shared/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User.Shared
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? Address { get; set; }
+        public string? CellNumber { get; set; }
+        public string? EmailId { get; set; }
+
+    }
+}
---
+++ WebApiUser/Models/UserBDContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiUser.Models
+{
+    public class UserBDContext : DbContext
+    {
+        public UserBDContext(DbContextOptions<UserBDContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User.Shared.User> Users { get; set; }
+    }
+}
